e10d22f24a4a85635a487f00d30e2767034f29bc
samuel.victor <samuel.victor@ebserh.gov.br>
2025-11-15 00:17:17 -0300

    Update instagram info

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,9 +2,6 @@ const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
   },
 
   // Always leave this as last one,
