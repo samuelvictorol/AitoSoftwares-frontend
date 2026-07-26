4793b330e41e40c102fe372b85c0c64f2982b921
samuelvictorol <samuel.softdev@outlook.com>
2026-07-25 21:45:54 -0300

    AitoCodex: restaura landing na rota raiz

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,7 @@ const routes = [
   {
     path: '/',
     name: 'landing-3d',
-    redirect: '/aitolearn',
+    component: () => import('pages/AitoLanding3DPage.vue'),
   },
   {
     path: '/auth/google/callback',
